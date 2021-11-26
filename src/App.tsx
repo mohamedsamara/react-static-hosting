af53ec4ddf5ff75e3bb725e2caf9d4eedce5ba95
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2021-11-25 20:23:47 -0800

    feat: add eslint & prettier & husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const App = () => {
-  return <div className="app"></div>;
+  return <div className='app'></div>;
 };
 
 export default App;
